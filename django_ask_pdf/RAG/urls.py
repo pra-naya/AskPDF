848aa00fd8af48a2f02f929e9ce2c8a5a180f38a
Pranaya Shrestha <pranaya.shrestha123@gmail.com>
2024-11-15 07:14:22 +0545

    Minor changes

--- django_ask_pdf/RAG/urls.py
+++ django_ask_pdf/RAG/urls.py
@@ -14,9 +14,9 @@ urlpatterns = [
     path('delete/<int:file_id>', views.delete, name='delete'),
 ]
 
-# # This part allows Django to serve media files during development.
-# from django.conf import settings
-# from django.conf.urls.static import static
+# This part allows Django to serve media files during development.
+from django.conf import settings
+from django.conf.urls.static import static
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
